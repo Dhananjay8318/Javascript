46064ac87ba6505f46d51331417ccf844ff25573
dhananjay8318 <dhananjay.23b0131059@abes.ac.in>
2026-05-25 18:41:15 +0530

    comparison

---
+++ 04_comparision.js
@@ -0,0 +1,19 @@
+console.log(2>1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+
+console.log("2">1)//here  javascript automatic convert into a number
+console.log("02"<1)  
+
+console.log(null>0)
+console.log(null<0)
+console.log(null==0)
+console.log(null>=0) // it give a true 
+
+//strict check 
+
+//if we check == string and number then give a ture
+//if we check with ===  then its give a false
+
+console.log("33"===33)
