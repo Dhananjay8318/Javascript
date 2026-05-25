214a51c1a5ff86ae08cb6309fe87ce39277af7e6
dhananjay8318 <dhananjay.23b0131059@abes.ac.in>
2026-05-25 17:42:32 +0530

    conversion

---
+++ 03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score="33"
+
+console.log(typeof score)
+// another types is 
+console.log(typeof(score))
+
+// convert string to a number 
+
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+
+console.log(typeof valueInNumber)
+console.log(typeof score)
+
+
+// if we write a like "33abn" then you want to change in a number then it not change because it is not a number
+
+// "33"=> 33
+//"33sadd"=>NaN
+// true =>1 , false =>0
+
+// ""=> false
+// "dhananjay"=> true
