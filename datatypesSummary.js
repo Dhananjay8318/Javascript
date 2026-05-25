eac4d178efe465499e5831cee706171efc4027f5
dhananjay8318 <dhananjay.23b0131059@abes.ac.in>
2026-05-25 18:52:29 +0530

    datatypes

---
+++ datatypesSummary.js
@@ -0,0 +1,47 @@
+/*  primitive
+
+it is seven type 
+1:- String
+2:- Number
+3:- Boolean
+4:- null 
+5:- undefined
+6:- Symbol
+7:- BigInt
+
+*/
+
+// string data types
+ let name="dhananjay"
+
+//  number data types
+ let age=3
+
+//  boolean data types
+let isActive=false
+
+//Symbol
+let id=Symbol('1234')
+
+
+
+//reference (non primitive )
+
+/* 
+1:- Array
+2:- Objects
+3:- Functions
+*/
+
+// Array data types
+let numer=[1,2,3]
+
+//object 
+let myObj={
+    name: "dhananjay",
+    age:22,
+}
+//function
+let mufun=function(){
+console.log("hello")
+}
