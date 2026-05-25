58aa76fcda633da34e200ce99b23f4d041583138
dhananjay8318 <dhananjay.23b0131059@abes.ac.in>
2026-05-25 17:24:50 +0530

    Added JavaScript basics files

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const acId=4587
+let name="dhananjay"
+var rollNo=25
+// acId=457825    constant variable that cannot be reassingned
+// if we write any things in a let the can be reassingned
+// var is older way to declare variables
+console.log(acId)
+console.log(name)
+console.log(rollNo)
+
+// another method to print
+console.log([acId,name,rollNo])
+
+// var we not use because of issue in block scope and function scope . scope is {}
---
+++ 02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"  //treate all js code as newer version
+// alert(3+3)  we are useing node js not a browser
+
+console.log(3+3)
+console.log("dhananjay")
+
+// string data types
+ let name="dhananjay"
+
+//  number data types
+ let age=3
+
+//  boolean data types
+let isActive=false
+
+// Array data types
+let numer=[1,2,3]
+
+// bigint data types
+// null data types it is a stand lone value
+// undefined 
+// symbol => unique
+
+console.log([name,age,isActive,numer])
+
+// typeof it is use for knowing of data types 
+console.log(typeof name)
