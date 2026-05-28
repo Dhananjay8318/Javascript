141c57959784d9eee81f3b0623bbf093d4af9914
dhananjay8318 <dhananjay.23b0131059@abes.ac.in>
2026-05-28 11:53:25 +0530

    high order array loop

--- 18_objectfor.js
+++ 18_objectfor.js
@@ -17,3 +17,30 @@ const arr=["dhan","shiv","mohan","shayam"]
 arr.forEach(function(item){
     console.log(item)
 })
+
+
+//object in array
+
+const mycoding=[
+    {
+        name:"dhananjay",
+        roll:90
+    },
+    {
+        name:"ram",
+        roll:190
+    },
+    {
+        name:"shita",
+        roll:96
+    },
+    {
+        name:"shivam",
+        roll:258
+    }
+
+]
+
+mycoding.forEach(function(item){
+    console.log(item)
+})
